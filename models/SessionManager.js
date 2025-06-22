8ad48ad120f97a1e0e197f71949fb5d57a6b2192
kvssankar <kvs.sankar001@gmail.com>
2025-06-22 12:41:14 +0530

    session storage to dynamo added not yet tested

--- models/SessionManager.js
+++ models/SessionManager.js
@@ -19,10 +19,6 @@ import { FastIntentMatcher } from "../utils/FastIntentMatcher.js";
 
 const dynamoDbClient = new DynamoDBClient({
   region: process.env.AWS_REGION || "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
 const TABLE_NAME = "Sessions";
 const SESSION_TTL = 3600;
